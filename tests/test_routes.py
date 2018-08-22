c486948c316956ced0f07605955b76ab14d1d141
Collins Mbulakyalo <musannyanac@gmail.com>
2018-08-22 09:54:23 +0300

    v1
    Coompleted test coverage

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,2 +1,34 @@
+import unittest
 import pytest
 from app.routes import app, stack
+
+class TestForEndpoints(unittest.TestCase):
+    def setUp(self):
+        self.app = app
+        self.stack = stack
+        self.client = self.app.test_client
+
+    def test_for_hello_route(self):
+        res = self.client().get('/')
+        self.assertEqual(res.status_code, 200)
+        self.assertIsInstance(res.data, str)
+
+    def test_for_viewing_all_questions(self):
+        res = self.client().get('/questions')
+        self.assertEqual(res.status_code, 201)
+        self.assertIn('{}', res.data)
+
+    def test_for_adding_questions(self):
+        res = self.client().get('/questions')
+        self.assertEqual(res.status_code, 201)
+
+    def test_for_viewing_a_question(self):
+        res = self.client().get('/questions')
+        self.assertEqual(res.status_code, 201)
+
+    def test_for_adding_answers(self):
+        res = self.client().get('/questions')
+        self.assertEqual(res.status_code, 201)
+
+if __name__ == '__main__':
+    unittest.main()
