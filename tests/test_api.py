df10d604f789bf7bdca47945fe6429123742cc43
Collins Mbulakyalo <musannyanac@gmail.com>
2018-08-24 05:52:16 +0300

    Updates to tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -31,14 +31,17 @@ class TestForEndpoints(unittest.TestCase):
     def setUp(self):
         self.app = app
         self.client = self.app.test_client
+        self.quest = Questions()
+        self.ans = Answers()
 
     def test_for_hello_route(self):
         res = self.client().get('/')
         self.assertEqual(res.status_code, 200)
 
     def test_for_viewing_all_questions(self):
-        
+        self.quest.add_questions("What is a boolean?")
         res = self.client().get('/questions')
+        
         self.assertEqual(res.status_code, 201)
 
     def test_for_adding_questions(self):
