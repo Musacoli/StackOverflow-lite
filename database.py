7006b3fc14621ee4b4f30b6ae6ea7a29beb9db96
Collins Mbulakyalo <musannyanac@gmail.com>
2018-08-28 14:46:30 +0300

    Update to database

--- database.py
+++ database.py
@@ -19,46 +19,32 @@ class DatabaseConnection(object):
         self.cur.execute(create_users_table_command)
 
     def create_new_user(self, username, firstname, surname, email, password):
-        create_new_user_command = ("""INSERT INTO users 
+        create_new_user_command = ('''INSERT INTO users 
         (user_id, first_name, surname, email, password)
         VALUES (%s, %s, %s, %s, %s) 
-        """ % (username, firstname, surname, email, password))
+        ''' % (username, firstname, surname, email, password))
+        self.cur.execute(create_new_user_command)
 
     def create_questions_table(self):
-        create_questions_table_command = ('''CREATE TABLE users
-        (user_id TEXT PRIMARY KEY NOT NULL,
-        first_name TEXT NOT NULL,
-        surname TEXT NOT NULL,
-        email VARCHAR(20) NOT NULL,
-        password TEXT NOT NULL
-        ); ''')
-        self.cur.execute(create_questions_table_command)
-
-
-
-cur.execute('''CREATE TABLE users
-        (user_id TEXT PRIMARY KEY NOT NULL,
-        first_name TEXT NOT NULL,
-        surname TEXT NOT NULL,
-        email VARCHAR(20) NOT NULL,
-        password TEXT NOT NULL
-        );
-''')
-conn.commit()
-
-cur.execute('''CREATE TABLE questions
+        create_questions_table_command = ('''CREATE TABLE questions
         (question_id INT PRIMARY KEY NOT NULL,
         user_id TEXT NOT NULL,
         question_title TEXT NOT NULL,
         description TEXT NOT NULL,
         post_time TIMESTAMP,
         FOREIGN KEY(user_id) REFERENCES users.user_id
-        );
-''')
+        ); ''')
+        self.cur.execute(create_questions_table_command)
 
-conn.commit()
+    def create_a_question(self, question_id, user_id, title, description, post_time):
+        create_a_question_command = ('''INSERT INTO questions 
+        (question_id, user_id, question_title, description, post_time)
+        VALUES (%s, %s, %s, %s, %s) 
+        ''' % (question_id, user_id, title, description, post_time))
+        self.cur.execute(create_a_question_command)
 
-cur.execute('''CREATE TABLE answers
+    def create_an_answers_table(self):
+        create_an_answers_table_command = ('''CREATE TABLE answers
         (answer_id INT PRIMARY KEY NOT NULL,
         question_id INT NOT NULL,
         user_id TEXT NOT NULL,
@@ -67,50 +53,13 @@ cur.execute('''CREATE TABLE answers
         post_time TIMESTAMP,
         FOREIGN KEY(user_id) REFERENCES users.user_id,
         FOREIGN KEY(question_id) REFERENCES questions.question_id
-        );
-''')
-conn.commit()
+        ); ''')
+        self.cur.execute(create_an_answers_table_command)
 
-print ("Tables have been created successfully")
+    def create_an_answer(self, answer_id, question_id, user_id, title, description, post_time):
+        create_an_answer_command = ('''INSERT INTO answers 
+        (answer_id, question_id, user_id, answer_title, description, post_time)
+        VALUES (%s, %s, %s, %s, %s) 
+        ''' % (answer_id, question_id, user_id, title, description, post_time))
+        self.cur.execute(create_an_answer_command)
 
-"""cur.execute("INSERT INTO questions (question_id, user_id, question_title, description) \
- VALUES (3, 2, 'What is a boolean2?', 'I found this in a ceratin course')" );
-print("Information has been added")"""
-
-"""cur.execute("SELECT * FROM questions")
-rows = cur.fetchall()
-
-for row in rows:
-    print ("QID= ", row[0])
-    print ("UserId= ", row[1])
-    print ("Title= ", row[2])
-    print ("Descrp.= ", row[3]), "\n"
-    
-print ("operation successful")"""
-
-"""cur.execute(" UPDATE questions SET question_title = 'How can I append in python?' WHERE question_id = 2 ")
-cur.execute("SELECT * FROM questions")
-rows = cur.fetchall()
-
-for row in rows:
-    print ("QID= ", row[0])
-    print ("UserId= ", row[1])
-    print ("Title= ", row[2])
-    print ("Descrp.= ", row[3]), "\n"
-    
-print ("operation successful")"""
-
-"""cur.execute("DELETE FROM questions WHERE question_id = 2;")
-cur.execute("SELECT * FROM questions")
-rows = cur.fetchall()
-
-for row in rows:
-    print ("QID= ", row[0])
-    print ("UserId= ", row[1])
-    print ("Title= ", row[2])
-    print ("Descrp.= ", row[3]), "\n"
-    
-print ("operation successful")"""
-
-conn.commit()
-conn.close()
