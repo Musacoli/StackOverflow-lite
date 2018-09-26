46f66ab91c2b958788ff643561df2e52096ab80b
Collins Mbulakyalo <musannyanac@gmail.com>
2018-09-26 07:19:17 +0300

    Revert "Merge branch 'Interface' of https://github.com/Musacoli/StackOverflow-lite into Interface"
    This reverts commit 517e37724d1260f68c43b197b8ab9fd421e21b1d, reversing
    changes made to 2d7c2840bce82765afc1daadf9173b38ba479d71.

--- UI/scripts/fetch.js
+++ UI/scripts/fetch.js
@@ -10,15 +10,7 @@ function getQuestions() {
    // headers.append('Access-Control-Allow-Origin', '*');
    //  headers.append('Access-Control-Allow-Credentials', 'true');
    //  headers.append('Accept', 'application/json, text/plain, */*')
-<<<<<<< HEAD
    fetch('https://stackoverflow-lite-collo.herokuapp.com/questions') 
-=======
-<<<<<<< HEAD
-   fetch('https://stackoverflow-lite-collo.herokuapp.com/questions') 
-=======
-   fetch('http://127.0.0.1:5000/questions') 
->>>>>>> 820f4501a78258999d7daf44736dbeb34722aa3a
->>>>>>> c4f15d8156a666d3e993211ca2b04ef158c7fb37
    .then((res) => res.json())
    .then ((data) => {
        console.log(data);
@@ -51,15 +43,7 @@ function signUp(e) {
     let password = document.getElementById('password').value;
     let cpassword = document.getElementById('cpassword').value;
 
-<<<<<<< HEAD
     fetch('https://stackoverflow-lite-collo.herokuapp.com/signup', {
-=======
-<<<<<<< HEAD
-    fetch('https://stackoverflow-lite-collo.herokuapp.com/signup', {
-=======
-    fetch('http://127.0.0.1:5000/signup', {
->>>>>>> 820f4501a78258999d7daf44736dbeb34722aa3a
->>>>>>> c4f15d8156a666d3e993211ca2b04ef158c7fb37
         method: 'POST',
         headers: {
             'Accept': 'application/json, text/plain, */*',
@@ -85,15 +69,7 @@ function login(e) {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-<<<<<<< HEAD
     fetch('https://stackoverflow-lite-collo.herokuapp.com/auth/login', {
-=======
-<<<<<<< HEAD
-    fetch('https://stackoverflow-lite-collo.herokuapp.com/auth/login', {
-=======
-    fetch('http://127.0.0.1:5000/auth/login', {
->>>>>>> 820f4501a78258999d7daf44736dbeb34722aa3a
->>>>>>> c4f15d8156a666d3e993211ca2b04ef158c7fb37
         method: 'POST',
         headers: {
             'Accept': 'application/json, text/plain, */*',
